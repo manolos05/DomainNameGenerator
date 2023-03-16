fe70309c5d3f690e1e23aea19f2d6ca83b24b625
Manuel Salazar <manusalh@gmail.com>
2023-03-16 01:59:59 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#boton").addEventListener("click", () => {
+    document.querySelector("#generar").innerHTML = domain();
+  });
+};
+
+const domain = () => {
+  let pronoun = ["the", "our", "your"];
+  let adj = ["great", "big", "small"];
+  let noun = ["jogger", "racoon", "dog"];
+  let dot = [".cl", ".us", ".com", ".net"];
+  var hola = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let e = 0; e < noun.length; e++) {
+        for (let x = 0; x < dot.length; x++) {
+          hola += pronoun[i] + adj[j] + noun[e] + dot[x] + "<br>";
+          console.log(
+            pronoun[i]
+              .concat(adj[j])
+              .concat(noun[e])
+              .concat(dot[x])
+          );
+        }
+      }
+    }
+  }
+  return hola;
 };
